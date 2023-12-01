3db6a3abeedbaf1768881b29d8042c6b1f0b2ed2
Alexander Mandrov <otvet-1995@mail.ru>
2023-12-01 16:55:25 +0200

    add Filters

---
+++ components/Filter.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import * as React from "react";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+interface FilterProps {
+  filters: { name: string; value: string }[];
+  otherClasses?: string;
+  containerClasses?: string;
+}
+
+const Filter: React.FC<FilterProps> = ({
+  filters,
+  otherClasses,
+  containerClasses,
+}) => {
+  return (
+    <div className={`relative ${containerClasses}`}>
+      <Select>
+        <SelectTrigger
+          className={`${otherClasses} body-regular light-border background-light800_dark300 text-dark500_light700 border px-5 py-2.5`}
+        >
+          <div className="line-clamp-1 flex-1 select-none text-left">
+            <SelectValue placeholder="Select a Filter" />
+          </div>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {filters.map((filter) => (
+              <SelectItem
+                key={filter.value}
+                value={filter.value}
+                className="text-dark500_light700"
+              >
+                {filter.name}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
+
+export default Filter;
