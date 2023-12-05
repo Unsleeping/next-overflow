d5fa776a9602a106a2c926949a7bdb98370920c8
Alexander Mandrov <otvet-1995@mail.ru>
2023-12-06 00:36:46 +0200

    +

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -17,7 +17,7 @@ interface QuestionCardProps {
     name: string;
     picture: string;
   };
-  upvotes: number;
+  upvotes: string[];
   views: number;
   answers: Array<object>;
   createdAt: Date;
@@ -67,7 +67,7 @@ const QuestionCard: React.FC<QuestionCardProps> = ({
         <Metric
           imgUrl="/assets/icons/like.svg"
           alt="Upvotes"
-          value={formatAndDivideNumber(upvotes)}
+          value={formatAndDivideNumber(upvotes.length)}
           title={" Votes"}
           textStyles="small-medium text-dark400_light800"
         />
