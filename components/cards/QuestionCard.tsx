4c090c35bd988b30dae31ae995819b678349f3d4
Alexander Mandrov <otvet-1995@mail.ru>
2023-12-22 22:36:59 +0200

    +

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -40,64 +40,64 @@ const QuestionCard: React.FC<QuestionCardProps> = ({
 }) => {
   const showActionButtons = clerkId && clerkId === author.clerkId;
   return (
-    <Link href={`/question/${_id}`}>
-      <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
-        <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
-          <div>
-            <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
-              {getTimestamp(createdAt)}
-            </span>
+    <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
+      <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
+        <div>
+          <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
+            {getTimestamp(createdAt)}
+          </span>
 
+          <Link href={`/question/${_id}`}>
             <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
               {title}
             </h3>
-          </div>
+          </Link>
+        </div>
 
-          <SignedIn>
-            {showActionButtons && (
-              <EditDeleteAction type="Question" itemId={JSON.stringify(_id)} />
-            )}
-          </SignedIn>
-        </div>
-        <div className="mt-3.5 flex flex-wrap gap-2">
-          {tags.map((tag) => (
-            <RenderTag key={tag._id} name={tag.name} _id={tag._id} />
-          ))}
-        </div>
-        <div className="flex-between mt-6 w-full flex-wrap gap-3">
-          <Metric
-            imgUrl={author.picture}
-            alt="user"
-            value={author.name}
-            title={` - asked ${getTimestamp(createdAt)}`}
-            textStyles="body-medium text-dark400_light700"
-            href={`/profile/${author.clerkId}`}
-            isAuthor
-          />
-          <Metric
-            imgUrl="/assets/icons/like.svg"
-            alt="Upvotes"
-            value={formatAndDivideNumber(upvotes.length)}
-            title={" Votes"}
-            textStyles="small-medium text-dark400_light800"
-          />
-          <Metric
-            imgUrl="/assets/icons/message.svg"
-            alt="message"
-            value={formatAndDivideNumber(answers.length)}
-            title={" Answers"}
-            textStyles="small-medium text-dark400_light800"
-          />
-          <Metric
-            imgUrl="/assets/icons/eye.svg"
-            alt="eye"
-            value={formatAndDivideNumber(views)}
-            title={" Views"}
-            textStyles="small-medium text-dark400_light800"
-          />
-        </div>
+        <SignedIn>
+          {showActionButtons && (
+            <EditDeleteAction type="Question" itemId={JSON.stringify(_id)} />
+          )}
+        </SignedIn>
       </div>
-    </Link>
+      <div className="mt-3.5 flex flex-wrap gap-2">
+        {tags.map((tag) => (
+          <RenderTag key={tag._id} name={tag.name} _id={tag._id} />
+        ))}
+      </div>
+      <div className="flex-between mt-6 w-full flex-wrap gap-3">
+        <Metric
+          imgUrl={author.picture}
+          alt="user"
+          value={author.name}
+          title={` - asked ${getTimestamp(createdAt)}`}
+          textStyles="body-medium text-dark400_light700"
+          href={`/profile/${author.clerkId}`}
+          isAuthor
+        />
+        <Metric
+          imgUrl="/assets/icons/like.svg"
+          alt="Upvotes"
+          value={formatAndDivideNumber(upvotes.length)}
+          title={" Votes"}
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/message.svg"
+          alt="message"
+          value={formatAndDivideNumber(answers.length)}
+          title={" Answers"}
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/eye.svg"
+          alt="eye"
+          value={formatAndDivideNumber(views)}
+          title={" Views"}
+          textStyles="small-medium text-dark400_light800"
+        />
+      </div>
+    </div>
   );
 };
 
